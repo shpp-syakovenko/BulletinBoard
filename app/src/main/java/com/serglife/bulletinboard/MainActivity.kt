0078c460b96f435d7b162950f5ea84dd1f438a01
Sergey <serglife777@gmail.com>
2022-06-17 16:32:05 +0300

    connection to firebase

--- app/src/main/java/com/serglife/bulletinboard/MainActivity.kt
+++ app/src/main/java/com/serglife/bulletinboard/MainActivity.kt
@@ -2,7 +2,6 @@ package com.serglife.bulletinboard
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Gravity
 import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
