b3e6b50264cca50cfe70db0605f2fe6d5cb9833e
Sergey <serglife777@gmail.com>
2022-06-25 21:19:43 +0300

    Add Countries in spinner(new token)

--- app/src/main/java/com/serglife/bulletinboard/ui/edit/EditAdsAct.kt
+++ app/src/main/java/com/serglife/bulletinboard/ui/edit/EditAdsAct.kt
@@ -7,7 +7,9 @@ import com.serglife.bulletinboard.databinding.ActivityEditAdsBinding
 import com.serglife.bulletinboard.utils.CityHelper
 
 class EditAdsAct : AppCompatActivity() {
+
     private lateinit var binding: ActivityEditAdsBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(ActivityEditAdsBinding.inflate(layoutInflater).also { binding = it }.root)
