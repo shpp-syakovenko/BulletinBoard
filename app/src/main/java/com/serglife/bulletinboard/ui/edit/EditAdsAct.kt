5ecd4dc24882b2652889149d4de71cf5b3a15cd0
Sergey <serglife777@gmail.com>
2022-07-08 21:42:39 +0300

    add pixImagePiker

--- app/src/main/java/com/serglife/bulletinboard/ui/edit/EditAdsAct.kt
+++ app/src/main/java/com/serglife/bulletinboard/ui/edit/EditAdsAct.kt
@@ -1,13 +1,19 @@
 package com.serglife.bulletinboard.ui.edit
 
+import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Toast
+import com.fxn.pix.Pix
+import com.fxn.utility.PermUtil
 import com.serglife.bulletinboard.R
 import com.serglife.bulletinboard.databinding.ActivityEditAdsBinding
 import com.serglife.bulletinboard.ui.dialogs.country.DialogSpinnerHelper
 import com.serglife.bulletinboard.utils.CityHelper
+import com.serglife.bulletinboard.utils.ImagePiker
 
 class EditAdsAct : AppCompatActivity() {
 
@@ -19,33 +25,68 @@ class EditAdsAct : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(ActivityEditAdsBinding.inflate(layoutInflater).also { binding = it }.root)
         init()
-
-
     }
 
-    private fun init(){
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(resultCode == RESULT_OK && requestCode == ImagePiker.REQUEST_CODE_GET_IMAGES){
+            if (data != null){
+                val valueReturn = data.getStringArrayListExtra(Pix.IMAGE_RESULTS)
+
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        when (requestCode) {
+            PermUtil.REQUEST_CODE_ASK_MULTIPLE_PERMISSIONS -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    ImagePiker.getImages(this)
+                } else {
+                    Toast.makeText(
+                        this,
+                        "Approve permissions to open Pix ImagePiker",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+        }
+    }
+
+    private fun init() {
         dialog = DialogSpinnerHelper()
 
     }
 
     //onClicks
-    fun onClickSelectedCountry(view:View){
+    fun onClickSelectedCountry(view: View) {
         val listCountry = CityHelper.getAllCountries(this)
         dialog.showSpinnerDialog(this, listCountry, binding.tvCountry)
-        if(binding.tvCity.text.toString() != getString(R.string.selected_city)){
+        if (binding.tvCity.text.toString() != getString(R.string.selected_city)) {
             binding.tvCity.text = getString(R.string.selected_city)
         }
     }
 
-    fun onClickSelectedCities(view:View){
+    fun onClickSelectedCities(view: View) {
 
         val selectedCountry = binding.tvCountry.text.toString()
 
-        if(selectedCountry != getString(R.string.selected_country)) {
+        if (selectedCountry != getString(R.string.selected_country)) {
             val listCities = CityHelper.getAllCities(this, selectedCountry)
             dialog.showSpinnerDialog(this, listCities, binding.tvCity)
-        }else{
-            Toast.makeText(this, getString(R.string.choose_country_first), Toast.LENGTH_SHORT).show()
+        } else {
+            Toast.makeText(this, getString(R.string.choose_country_first), Toast.LENGTH_SHORT)
+                .show()
         }
+    }
+
+    fun onClickGetImages(view:View){
+        ImagePiker.getImages(this)
     }
 }
